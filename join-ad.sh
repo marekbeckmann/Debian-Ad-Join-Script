dbcaa5c33015446606d10ce1699b6069fe654cb1
Marek Beckmann <80179488+marekbeckmann@users.noreply.github.com>
2021-10-26 12:46:41 +0200

    Create join-ad.sh

---
+++ join-ad.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+function init() {
+    if [ "$(whoami)" = "root" ]; then
+        if [ "$realm" != "" ]; then
+            installPackages
+            adJoin
+            summary
+        else
+            logToScreen "Please provide a REALM to join"
+            helpMsg
+            exit 1
+        fi
+    else
+        logToScreen "Please run as root" --error
+    fi
+}
+
+function summary() {
+    logToScreen "
+Succesfully joined the following Realm: 
+Realm: $realm
+Homedir: $homedir [/home/%u@%d]
+Default Shell: $shell [/bin/bash]
+Joined with: $adminuser
+"
+}
+
+function installPackages() {
+    apt -y update
+    apt -y install realmd sssd sssd-tools sssd-ad libnss-sss libpam-sss adcli samba-common-bin packagekit libpam-google-authenticator || logToScreen "Couldn't install Packages!" --error
+    systemctl stop sssd
+    cp /usr/lib/x86_64-linux-gnu/sssd/conf/sssd.conf /etc/sssd/.
+    chmod 600 /etc/sssd/sssd.conf
+    systemctl enable sssd
+    systemctl start sssd
+}
+
+function adJoin() {
+    logToScreen "Do you want to join the following REALM? To proceed, enter your credentials"
+    realm discover "$realm" || logToScreen "Can't discover realm" --error
+    realm join "$realm" -U "$adminuser" || logToScreen "Can't join AD" --error
+    if [ "$homedir" != "" ]; then
+        echo "override_homedir = $homedir" | tee -a /etc/sssd/sssd.conf
+    fi
+    if [ "$shell" != "" ]; then
+        sed -i '/default_shell/d' /etc/sssd/sssd.conf
+        echo "default_shell = $shell" | tee -a /etc/sssd/sssd.conf
+    fi
+    sed -i '/use_fully_qualified_names/d' /etc/sssd/sssd.conf
+    echo "use_fully_qualified_names = False" | tee -a /etc/sssd/sssd.conf
+    systemctl restart sssd
+    pam-auth-update --enable mkhomedir
+    id "$adminuser" || logToScreen "AD Join failed" --error
+}
+
+function logToScreen() {
+    clear
+    if [[ "$2" = "--success" ]]; then
+        printf '%s\n' "$(tput setaf 2)$1 $(tput sgr 0)"
+    elif [[ "$2" = "--error" ]]; then
+        printf '%s\n' "$(tput setaf 1)$1 $(tput sgr 0)"
+        exit 1
+    else
+        printf '%s\n' "$(tput setaf 3)$1 $(tput sgr 0)"
+    fi
+    sleep 1
+}
+function helpMsg() {
+    logToScreen "Help for AD Setup Script (Debian 10/11)
+You can use the following Options:
+  [-h] => Help Dialog
+  [-u] [--adminuser] => Admin User for authentication
+  [-d] [--ad-domain] => Realm you want to join
+  [-p] [--homedir] => Overrides the home directory path
+  [-s] [--shell] => Overrides the default shell
+
+More Documentation can be found on Github: https://github.com/marekbeckmann/debian-ad-join-script"
+}
+
+while test $# -gt 0; do
+    case "$1" in
+    -h | --help)
+        helpMsg
+        ;;
+    -u | --adminuser)
+        adminuser="$2"
+        ;;
+    -d | --ad-domain)
+        realm="$2"
+        ;;
+    -p | --homedir)
+        homedir="$2"
+        ;;
+    -s | --shell)
+        shell="$2"
+        ;;
+    --*)
+        logToScreen "Unknown option $1" --error
+        helpMsg
+        exit 1
+        ;;
+    -*)
+        logToScreen "Unknown option $1" --error
+        helpMsg
+        exit 1
+        ;;
+    esac
+    shift
+done
